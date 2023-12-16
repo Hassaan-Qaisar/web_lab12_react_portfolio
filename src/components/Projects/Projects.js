2230fe9613beba5c0440f7621f49b1c4f36d44e6
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2023-12-17 01:11:30 +0500

    Update project links to Hassaan Qaisar's projects. (generated by blackbox.ai )

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,8 +27,8 @@ function Projects() {
               isBlog={false}
               title="ECommerce Website using NextJS"
               description="Experience the future of online shopping with our E-Commerce Website powered by Next.js. Enjoy features like user authentication, product catalog, shopping cart, and a seamless checkout process. Explore a responsive design that adapts to your device, making shopping on-the-go a breeze."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              ghLink="https://github.com/Hassaan-Qaisar/ecommerce_nextjs"
+              demoLink="https://ecommerce-nextjs-beige.vercel.app/"
             />
           </Col>
 
@@ -38,8 +38,8 @@ function Projects() {
               isBlog={false}
               title="Real Estate Website using MERN Stack"
               description="Experience seamless property management and exploration with our intuitive Real Estate Website, powered by the MERN Stack, offers easy Google Sign-In, property searches, visit scheduling, and property listing. Users can also mark favorites. It's your all-in-one real estate hub!"
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              ghLink="https://github.com/Hassaan-Qaisar/real_estate"
+              demoLink="https://real-estate-ten-black.vercel.app/"
             />
           </Col>
 
@@ -49,8 +49,8 @@ function Projects() {
               isBlog={false}
               title="Blog Website using NextJS"
               description="Explore my insights into the software industry with ease on this Next.js-powered Blog Website. Read articles on various tools and technologies, and reach out to me through the contact form for any inquiries."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              ghLink="https://github.com/Hassaan-Qaisar/blog-website"
+              demoLink="https://blog-website-omega-flame.vercel.app/"              
             />
           </Col>
 
@@ -60,8 +60,8 @@ function Projects() {
               isBlog={false}
               title="Contact App using ReactJS"
               description="Stay organized with my React-based Contact Saving App. Easily manage your contacts by adding, editing, and deleting entries. Quickly find the information you need with the search feature, making contact management a breeze."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              ghLink="https://github.com/Hassaan-Qaisar/contacts-react"
+              demoLink="https://contacts-react-eta.vercel.app/"
             />
           </Col>
 
