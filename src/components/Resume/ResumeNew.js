4033272337f14ecd6ab52bff0edad1985b6faac8
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2023-12-17 00:57:35 +0500

    initial commit

---
+++ src/components/Resume/ResumeNew.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+function ResumeNew() {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  const handleDownloadClick = () => {
+    // Redirect the user to the specified link
+    window.open(
+      "https://drive.google.com/file/d/1Z8Xq_OnW4d_bFompYyTbUuCFQqWz9L8T/view",
+      "_blank"
+    );
+  };
+
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            onClick={handleDownloadClick}
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
